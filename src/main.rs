83ef8bd621f06a5e97ef1abc241b91436dff771f
Troy Dai <git@troydai.com>
2024-08-19 22:11:50 -0700

    Terrible way to accept connections

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
 };
 
@@ -19,9 +19,31 @@ fn main() -> std::io::Result<()> {
 }
 
 fn handle(worker_id: i32, stream: &mut TcpStream, addr: SocketAddr) {
-    println!("Worker {} accept connection from {}", worker_id, addr);
+    let resp = format!("Worker {} accept connection from {}", worker_id, addr);
 
-    stream
-        .write(b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello")
-        .unwrap();
+    match stream.write(resp.as_bytes()) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Failed to write to stream: {}", e);
+            return;
+        }
+    }
+    let mut buf = [0u8; 1024];
+    loop {
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    println!("[{}] Connection closed", worker_id);
+                    return;
+                }
+
+                let data = &buf[..n];
+                print!("[{}] Received data: {}", worker_id, String::from_utf8_lossy(data));
+            }
+            Err(e) => {
+                println!("[{}] Failed to read from stream: {}", worker_id, e);
+                return;
+            }
+        }
+    }
 }
