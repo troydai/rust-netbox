6c4df5edc4e79f18ba3c513aaebf8f8fea253a10
Troy Dai <git@troydai.com>
2024-08-19 20:48:03 -0700

    Skeleton TCP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::Write,
+    net::{SocketAddr, TcpListener, TcpStream},
+};
+
+fn main() -> std::io::Result<()> {
+    let lis = TcpListener::bind("127.0.0.1:3000")?;
+
+    let mut worker_id = 0;
+    loop {
+        match lis.accept() {
+            Ok((mut socket, addr)) => handle(worker_id, &mut socket, addr),
+            Err(e) => {
+                println!("Failed to accept connection: {}", e)
+            }
+        }
+        worker_id += 1;
+    }
+}
+
+fn handle(worker_id: i32, stream: &mut TcpStream, addr: SocketAddr) {
+    println!("Worker {} accept connection from {}", worker_id, addr);
+
+    stream
+        .write(b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello")
+        .unwrap();
 }
